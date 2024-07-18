98b8c1d6ed6837369c77d64a2b19b006030ea638
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-18 01:59:33 -0400

    finished the how it works section

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -4,7 +4,7 @@ import bonfire from '../assets/bonfire.jpeg'
 
 function Hero() {
   return (
-    <section className='flex flex-col justify-center min-h-screen'>
+    <section className='flex flex-col justify-center min-h-screen -mt-3 max-w-[1500px] mx-auto'>
       <div className='flex flex-col gap-[110px] justify-center items-center my-auto max-w-[1500px]'>
         <div className='grid grid-cols-1 place-items-center gap-5 xl:flex xl:items-center xl:justify-between xl:w-[70%]'>
           <div className='text-white text-3xl sm:text-4xl'>
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbutton from './navbutton'
+import Navbutton from './inner-components/navbutton'
 
 function Navbar() {
 
