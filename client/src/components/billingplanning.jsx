44fddca7311a3c55e6ccdf37d177a50f2383f65d
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-18 21:28:01 -0400

    copied template to sub plan and made both of them forms with post methods

--- client/src/components/billingplanning.jsx
+++ client/src/components/billingplanning.jsx
@@ -12,7 +12,7 @@ function BillingPlanning() {
   return (
     <div className='text-white min-h-screen grid grid-cols-1 lg:grid-cols-2 max-w-[1000px] mx-auto
     gap-5'>
-      <div className='flex flex-col p-5 border h-[345px] rounded-xl gap-4'>
+      <form action="" method="POST" className='flex flex-col p-5 border h-[345px] rounded-xl gap-4'>
         <div className='flex justify-between'>
           <div className='text-xl font-extrabold'>Prepaid Plan</div>
           <div className='flex items-center gap-2 text-xl'>
@@ -25,7 +25,7 @@ function BillingPlanning() {
             <div className='font-semibold'>Plan Details</div>
             <div className='flex items-center gap-2 font-thin'>
               <i className="fa-solid fa-check"></i>
-              <div>20 Fast Prepaid API calls</div>
+              <div>20 fast prepaid API calls</div>
             </div>
             <div className='flex items-center gap-2 font-thin'>
               <i className="fa-solid fa-check"></i>
@@ -58,11 +58,55 @@ function BillingPlanning() {
             <div>Checkout with: </div>
           </button>
         </div>
-      </div>
+      </form>
 
-      <div className='flex flex-col p-5 h-[345px] border rounded-xl'>
-
-      </div>
+      <form action='' method="POST" className='flex flex-col p-5 border h-[345px] rounded-xl gap-4'>
+        <div className='flex justify-between'>
+          <div className='text-xl font-extrabold'>Subscription Plan</div>
+          <div className='flex items-center gap-2 text-xl'>
+            <i className="fa-regular fa-circle-check text-[15px]"></i>
+            <div>Active</div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <div className='flex flex-col'>
+            <div className='font-semibold'>Plan Details</div>
+            <div className='flex items-center gap-2 font-thin'>
+              <i className="fa-solid fa-check"></i>
+              <div>Pay per usage with song requests</div>
+            </div>
+            <div className='flex items-center gap-2 font-thin'>
+              <i className="fa-solid fa-check"></i>
+              <div>Unlimited API calls</div>
+            </div>
+            <div className='flex items-center gap-2 font-thin'>
+              <i className="fa-solid fa-check"></i>
+              <div>Endless beats</div>
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            <div className='font-semibold'>Billing & Payment</div>
+            <div className='flex justify-between font-thin'>
+              <div>Price</div>
+              <div>0.05&#162; / call</div>
+            </div>
+            <div className='flex justify-between font-thin'>
+              <div>Billing Period</div>
+              <div>Monthly</div>
+            </div>
+            <div className='flex justify-between font-thin'>
+              <div>Renewal Date</div>
+              <div>August 21, 2024</div>
+            </div>
+          </div>
+        </div>
+        <div className='flex justify-start'>
+          <button className='flex items-center font-semibold gap-2 bg-black py-2 px-4 rounded-xl hover:bg-white hover:text-black duration-200'>
+            <i className="fa-solid fa-cart-shopping"></i>
+            <div>Checkout with: </div>
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
