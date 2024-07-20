9c242493b4c4ecd764c14672aaf6abecaf0b313f
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-20 14:20:06 -0400

    correct retrieval of information after requesting API status

--- client/src/components/statuscheck.jsx
+++ client/src/components/statuscheck.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function StatusCheck() {
 
   const serverDOMAIN = 'http://localhost:5000'
 
-  const [APIValue, setAPIValue] = useState(null)
+  const [APIValue, setAPIValue] = useState("")
+
+  const [statusDisplay, setStatusDisplay] = useState(null)
 
   async function checkStatus() {
     if(!APIValue) {
@@ -14,8 +16,23 @@ function StatusCheck() {
       const res = await fetch(`${serverDOMAIN}/check-status/${APIValue}`)
       const { status } = await res.json()
       console.log(status)
+      // configuring status display
+      if(status === "API key does not exist.") {
+        setStatusDisplay(<div className='text-red-500'>API key does not exist.</div>)
+      } else if (status === null) {
+        setStatusDisplay(<div className='text-red-500'>Inactive key.</div>)
+      } else if (status === "subscription") {
+        setStatusDisplay(<div className='text-blue-300'>Subscription status: <span className='text-green-400'>ACTIVE</span></div>)
+      } else {
+        setStatusDisplay(<div className='text-blue-300'>API calls left: <span className='text-green-400'>{status}</span></div>)
+      }
     }
   }
+
+  // this returns the display messages to nothing everytime the page refreshes.
+  useEffect(() => {
+    setStatusDisplay(null)
+  }, [])
 
   return (
     <div id='Status-Check' className='h-[225px] bg-[#2e2860] w-full'>
@@ -28,7 +45,7 @@ function StatusCheck() {
             <div>alksdjfSDK3edkjsADDD</div>
           </div>
         </div>
-        <div className='flex justify-around w-[50%]'>
+        <div className='flex justify-around w-[50%] -mb-2'>
           <input 
             placeholder='Enter API Key...' 
             className='w-[50%] xl:w-[80%] rounded-full p-3 text-black'
@@ -38,6 +55,7 @@ function StatusCheck() {
           <button onClick={checkStatus} className='bg-[#5147a5] p-2 rounded-xl hover:opacity-65'>Check Status</button>
         </div>
         {/* This is the room to render variable status statements */}
+        {statusDisplay}
       </div>
     </div>
   )
