53c5e777218fd0da1655282446f8eea47cadb3d3
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-21 13:58:31 -0400

    basic layout for the docs has been created, no variables yet

--- client/src/components/docs.jsx
+++ client/src/components/docs.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 function Docs() {
   return (
     <div>
-      Hello
+      <div className='fixed top-0 left-0 min-h-screen w-[300px]  border-r-[1px] border-[#dfdfdf]'>
+        <div className='flex flex-col py-8 px-6'>
+          <div className='flex flex-col gap-5'>
+            <div className='flex items-center gap-2 text-xl sm:text-2xl xl:text-3xl'>
+              <div>Music API</div>
+              <i className="fa-solid fa-music"></i>
+            </div>
+            <div className='text-sm sm:text-lg xl:text-xl'>Music API <span className='text-[#3030308d]'>&#47; Docs</span></div>
+          </div>
+        </div>
+        <div className='h-[1px] w-full bg-[#dfdfdf]'></div>
+        <div className='flex flex-col border-b-[1px]'>
+          <div className='flex items-center h-[50px] px-6 hover:bg-gray-100'>Docs Home</div>
+          <div className='flex items-center h-[50px] px-6 hover:bg-gray-100'> Instructions</div>
+          <div className='flex items-center h-[50px] px-6 hover:bg-gray-100'>Endpoints</div>
+        </div>
+      </div>
+      <div className='ml-[400px] my-[100px]'>
+        Body contents
+      </div>
     </div>
   )
 }
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -8,13 +8,14 @@ function Navbar() {
 
   return (
     <nav className='flex justify-between items-start p-5 text-white'>
-      <div className='flex flex-col gap-2 items-center'>
+      <div className='flex flex-col gap-2 items-start'>
         <div className='flex items-center gap-2 text-lg sm:text-xl xl:text-2xl'>
           <div>Music API</div>
           <i className="fa-solid fa-music"></i>
         </div>
         <NavLink to='/docs'>
-          <button className='flex gap-2 items-center bg-blue-950 text-green-400 p-2 rounded-xl'>
+          <button className='flex gap-2 items-center bg-blue-950 text-green-400 p-2 rounded-xl -ml-2
+          hover:opacity-75'>
             <div>Docs</div>
             <i class="fa-solid fa-book"></i>
           </button>
